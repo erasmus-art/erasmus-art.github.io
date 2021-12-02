de0a0c24055337942491061e2c9af2c263389b0e
Boyan-Apostolov <boian4934@gmail.com>
2021-12-02 17:46:12 +0200

    Add page about bulgaria

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
 
 // activities drowdown
-document.querySelector('.activities-dropdown-toggle').addEventListener('click', () => {
-    var dropdown = document.querySelector('.activities-dropdown-menu');
-    var toggleButton = document.querySelector('.button-dropdown i');
+if (document.querySelector('.activities-dropdown-toggle')) {
+    document.querySelector('.activities-dropdown-toggle').addEventListener('click', () => {
+        var dropdown = document.querySelector('.activities-dropdown-menu');
+        var toggleButton = document.querySelector('.activities-dropdown-toggle i');
 
-    if (dropdown.style.display == 'block') {
-        dropdown.style.display = 'none';
-        toggleButton.classList.remove('fa-caret-up');
-        toggleButton.classList.add('fa-caret-down');
-    } else {
-        dropdown.style.display = 'block';
-        toggleButton.classList.remove('fa-caret-down');
-        toggleButton.classList.add('fa-caret-up');
-    }
-})
+        if (dropdown.style.display == 'block') {
+            dropdown.style.display = 'none';
+            toggleButton.classList.remove('fa-caret-up');
+            toggleButton.classList.add('fa-caret-down');
+        } else {
+            dropdown.style.display = 'block';
+            document.querySelector('.partners-dropdown-menu').style.display = 'none';
+            toggleButton.classList.remove('fa-caret-down');
+            toggleButton.classList.add('fa-caret-up');
+        }
+    })
+}
+
+
+//partners dropdown
+if (document.querySelector('.partners-dropdown-toggle')) {
+    document.querySelector('.partners-dropdown-toggle').addEventListener('click', () => {
+        var dropdown = document.querySelector('.partners-dropdown-menu');
+        var toggleButton = document.querySelector('.partners-dropdown-toggle i');
+
+        if (dropdown.style.display == 'block') {
+            dropdown.style.display = 'none';
+            toggleButton.classList.remove('fa-caret-up');
+            toggleButton.classList.add('fa-caret-down');
+        } else {
+            dropdown.style.display = 'block';
+            document.querySelector('.activities-dropdown-menu').style.display = 'none';
+            toggleButton.classList.remove('fa-caret-down');
+            toggleButton.classList.add('fa-caret-up');
+        }
+    })
+}
