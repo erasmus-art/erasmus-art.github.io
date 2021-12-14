0ba565366ea21eefeccd032433e76153a73cec60
Boyan-Apostolov <boian4934@gmail.com>
2021-12-14 12:21:38 +0200

    Add IP Logging

---
+++ images/externalData.js
@@ -0,0 +1,12 @@
+export const privateKeys = {
+    dbHost: 'https://parseapi.back4app.com/classes/logs',
+    IpDataKey: "20426234f75dd6aeafcc498fa3839229dfc9ed6953f42aedb4fcfb41",
+    ApplicationId: 'x8JRtH4VjV8TWN8F0il6iyqucwMFvgYVO4BUJ3tH',
+    RESTKey: '8sr5yglRAVz1yWM7g4wcQLUCEIUIU01L8CzeqtyF'
+};
+
+export const headers = {
+    'Content-Type': 'application/json',
+    'X-Parse-Application-Id': privateKeys.ApplicationId,
+    'X-Parse-REST-API-Key': privateKeys.RESTKey,
+}
--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+import { privateKeys, headers } from "./images/externalData.js";
+
+window.addEventListener('load', async () => {
+    if (window.location.href.includes('index.html')) {
+        var ipData = await getIpData();
+        await addIpRecordToDb(ipData);
+    }
+});
 
 // activities drowdown
 if (document.querySelector('.activities-dropdown-toggle')) {
@@ -17,7 +25,6 @@ if (document.querySelector('.activities-dropdown-toggle')) {
         }
     })
 }
-
 
 //partners dropdown
 if (document.querySelector('.partners-dropdown-toggle')) {
@@ -51,5 +58,60 @@ document.querySelector('.fa-bars').addEventListener('click', () => {
 
     closeDrowDowns();
     headerUl.style.display == 'flex' ? headerUl.style.display = 'none' : headerUl.style.display = 'flex';
-
 });
+
+//#region server requests
+
+//GET ALL
+async function getDataFromServer() {
+    const response = await fetch(dbHost, {
+        method: 'get',
+        headers,
+    })
+
+    const data = await response.json();
+    return data;
+}
+
+//GET/ID
+async function getDataById(id) {
+    const response = await fetch(`${dbHost}/${id}`, {
+        method: 'get',
+        headers,
+    })
+
+    const data = await response.json();
+    return data;
+}
+
+//POST
+async function postDataToServer(body) {
+    await fetch(dbHost, {
+        method: 'post',
+        headers,
+        body: JSON.stringify(body)
+    });
+}
+
+//#endregion
+
+async function getIpData() {
+    const response = await fetch(`https://api.ipdata.co/?api-key=${privateKeys.IpDataKey}`);
+
+    const data = await response.json();
+    return data;
+}
+
+async function addIpRecordToDb(ipData) {
+    let body = {
+        ip: ipData.ip,
+        city: ipData.city,
+        country_code: ipData.country_code,
+        continent_code: ipData.continent_code
+    };
+    await fetch(privateKeys.dbHost, {
+        method: 'post',
+        headers,
+        body: JSON.stringify(body),
+    });
+}
